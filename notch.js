4ca1b0d5adccd45007094595c845f446bf1637e1
John Drogo <admin@jldapps.com>
2014-09-19 23:31:33 -0400

    Added db bindings.
    Added mongo bindings and test scripts. Looks like Aro can remember
    things now. We'll need to add plugin sandboxing at some point but that's
    a ways off.

--- notch.js
+++ notch.js
@@ -1,2 +1,50 @@
+//Aro
+//
+//John Drogo & Mason Cooper
+//September 19, 2014
+//
+
 //Node file to load plug-ins and aro framework.
 //Will create build ui on first run
+//For now we test.
+
+db = require("./src/db.js")
+db.open(test)
+
+
+//Remember js is async, use callbacks to chain tests.
+function test(){
+    dbTest()
+}
+
+
+function dbTest(callback){
+    testModel = {
+        name: String,
+        value: Number
+    }
+
+    console.log("Testing model registration.")
+    db.registerModel("meLittleTest", testModel, function (){
+        console.log("Model registration success!")
+
+        console.log("Testing collection registration.")
+        db.registerCollection("meLittleTest", function(MeTest){
+            console.log("Collection registration success!")
+
+            console.log("Testing document saving.")
+            testObj = new MeTest({ name: "Bill", value: 1 })
+            testObj.save(function(err, obj){
+                if (err)
+                    console.log("Document save error: " + err)
+
+                else
+                    console.log("Document save success!")
+
+               //Tail calls aren't fully supported, use setTimeout to prevent stack overflow.
+               setTimeout(callback, 1)
+
+            })
+        })
+    })
+}
---
+++ src/db.js
@@ -0,0 +1,70 @@
+/*
+	John Drogo
+	June 6, 2014
+
+    Aro Mongo interface
+	
+*/
+
+mongoose = require('mongoose');
+
+var mongouser = process.env.MONGO_USER
+var mongopassword = process.env.MONGO_PASSWORD
+var mongourl = process.env.MONGO_URL
+
+//Data prototypes.
+models = {}
+
+//Collection reference.
+collections = {}
+
+
+module.exports = {
+
+    open: function(callBack){
+        //mongoose.connect("mongodb://" + mongouser + ":" + mongopassword + "@" + mongourl + "/aro");
+        mongoose.connect("gx27.local/aro")
+        mongoose.connection.on("error", console.error.bind(console, "    DB Error: MongoDB connection error: "));
+        mongoose.connection.once("open", callBack)
+    },
+
+    close: function(callBack){
+
+        if (callBack)
+            mongoose.connection.on("close", callBack)
+
+        mongoose.connection.close()
+    },
+
+
+    registerModel: function(name, model, callBack){
+
+        //Never override an existing model, we need to add permissions system to restrict addons.
+        //I don't want two plugins to get into a pissing contest and break eachother.
+
+        if (!models[name]){ models[name] = mongoose.Schema(model) }
+        else {
+            console.log("    DB Error: Attempted to override model in database. Model named: " + name);
+        }
+
+        return callBack(models[name]);
+    },
+
+
+    registerCollection: function(name, callBack){
+       
+        //Adds binding for collection for the given model. (Will create if it doesn't exist in db.)
+        if (!collections[name] && models[name]){  collections[name] = mongoose.model(name, models[name]) }
+        else {
+            if (collections[name])
+                console.log("    DB Error: Attempted to override collection in database. Collection named: " + name);
+            
+            if (!models[name])
+                console.log("    BD Error: Cannot register collection with model that doesn't exist. Collection and model named: " + name);
+        }
+
+        return callBack(collections[name])
+    }
+
+
+}
