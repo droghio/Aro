765d15af0873cef7c8d4e59724d3cec85271fa20
John Drogo <admin@jldapps.com>
2014-11-24 21:05:54 -0500

    More fixes.
    Templates now look pretty good and will load eachother. Figured out how
    to click hijack in the iframe. Need to grab click sender and search for
    "aro" attributes and present a modal to allow editing of content.
    
    Then need to figure out how to sync this back to the server of course,
    but that should be too bad... Right?????

---
+++ aro_quiver/admin/pages/clientadmin.js
@@ -0,0 +1,14 @@
+﻿//
+// John Drogo
+// 
+// November 24, 2014
+//
+// Client Admin Connection
+//
+// Oh boy this guy's gonna be big.
+// Basicially it needs to coordinate with the server,
+// grab data, render it, allow live editing, negotiate
+// login credientials and then reupload the file.
+//
+// ....
+
